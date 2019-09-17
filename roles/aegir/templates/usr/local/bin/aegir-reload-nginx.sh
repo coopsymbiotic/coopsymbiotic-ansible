59c84c08698ca8e8c66968090d6aa9c966c4419c
Mathieu Lutfy <mathieu@symbiotic.coop>
2019-09-17 16:31:50 -0400

    aegir: better duct tape for the nginx reload problem (previous sudo rule did not work)

---
+++ roles/aegir/templates/usr/local/bin/aegir-reload-nginx.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is a workaround script so that we can safely call it with sudo
+# and because on Debian10, mysteriously, "systemctl reload nginx" ends
+# up exiting nginx if there is a syntex error.
+/usr/sbin/nginx -g 'daemon on; master_process on;' -s reload

1fcaf860b155c8cd2e850c3a624e3a58bc018eaa
Mathieu Lutfy <mathieu@symbiotic.coop>
2019-10-15 17:52:05 -0400

    aegir: add templates/usr/local/bin/fix-wordpress-permissions.sh

---
+++ roles/aegir/templates/usr/local/bin/fix-wordpress-permissions.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# {{ ansible_managed }}
+
+# Help menu
+print_help() {
+cat <<-HELP
+This script is used to fix the file permissions of a WordPress site. You need
+to provide the following argument:
+
+  --site-path: Path to the WordPress site's directory.
+
+Usage: (sudo) ${0##*/} --site-path=PATH
+Example: (sudo) ${0##*/} --site-path=/var/aegir/platforms/wordpress/sites/example.org
+HELP
+exit 0
+}
+
+site_path=${1%/}
+
+# Parse Command Line Arguments
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --site-path=*)
+        site_path="${1#*=}"
+        ;;
+    --help) print_help;;
+    *)
+      printf "Error: Invalid argument, run --help for valid arguments.\n"
+      exit 1
+  esac
+  shift
+done
+
+if [ -z "${site_path}" ] || [ ! -d "${site_path}/wp-content" ] ; then
+  printf "Error: Please provide a valid WordPress site directory.\n"
+  exit 1
+fi
+
+if [ $(id -u) != 0 ]; then
+  printf "Error: You must run this with sudo or root.\n"
+  exit 1
+fi
+
+cd $site_path
+
+if [ -d ./wp-content/uploads/civicrm ]; then
+  chown -R aegir.www-data ./wp-content/uploads/civicrm/
+  chmod -R g+w ./wp-content/uploads/civicrm/
+fi

79e5e807268d150e7b38d5805235f45f72a8c89f
Mathieu Lutfy <mathieu@symbiotic.coop>
2020-10-07 16:09:22 -0400

    aegir: sudo/script so that the www-data user can see cron logs

--- roles/aegir/templates/usr/local/bin/aegir-reload-nginx.sh
+++ roles/aegir/templates/usr/local/bin/aegir-reload-nginx.sh
@@ -2,5 +2,5 @@
 
 # This is a workaround script so that we can safely call it with sudo
 # and because on Debian10, mysteriously, "systemctl reload nginx" ends
-# up exiting nginx if there is a syntex error.
+# up exiting nginx if there is a syntax error.
 /usr/sbin/nginx -g 'daemon on; master_process on;' -s reload

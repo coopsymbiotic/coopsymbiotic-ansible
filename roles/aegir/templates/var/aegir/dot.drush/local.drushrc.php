7a290e72f1de698af0dc0554a21bbd4d3774c5bc
Mathieu Lutfy <mathieu@symbiotic.coop>
2021-09-24 14:04:06 -0400

    aegir: disable database cloaking for d9

--- roles/aegir/templates/var/aegir/dot.drush/local.drushrc.php
+++ roles/aegir/templates/var/aegir/dot.drush/local.drushrc.php
@@ -6,3 +6,6 @@
 // when Aegir is updated and runs verify on all platforms (which then updates
 // all platforms).
 $options['provision_composer_install_platforms'] = FALSE;
+
+// https://www.drupal.org/project/provision/issues/2953349#comment-14038063
+$options['provision_db_cloaking'] = FALSE;

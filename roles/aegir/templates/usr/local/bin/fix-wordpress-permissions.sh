2cf5c75e63a69f7a303f0c7e8497750954087e15
Mathieu Lutfy <mathieu@symbiotic.coop>
2023-05-16 10:42:46 -0400

    aegir: fix-wordpress-permissions.sh: add condition to avoid confusing warnings on the console

--- roles/aegir/templates/usr/local/bin/fix-wordpress-permissions.sh
+++ roles/aegir/templates/usr/local/bin/fix-wordpress-permissions.sh
@@ -50,8 +50,10 @@ chmod 0770 $site_path
 chmod g+s $site_path
 
 # wp-config.php might not exist, ignore errors
-chown aegir.www-data wp-config.php || true
-chmod 0750 wp-config.php || true
+if [ -f wp-config.php ]; then
+  chown aegir.www-data wp-config.php
+  chmod 0750 wp-config.php
+fi
 
 # Relax permissions on wp-content, so that www-data can write in it
 # For example, WordFence requires this in order to create wp-content/wflogs

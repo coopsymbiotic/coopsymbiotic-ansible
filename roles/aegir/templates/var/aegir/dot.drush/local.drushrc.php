9a751b5f2d3fd9cc7401552534bfd2f017591494
Mathieu Lutfy <mathieu@symbiotic.coop>
2025-09-09 10:02:29 -0400

    aegir: local.drushrc.php: empty the file (not used anymore)

--- roles/aegir/templates/var/aegir/dot.drush/local.drushrc.php
+++ roles/aegir/templates/var/aegir/dot.drush/local.drushrc.php
@@ -2,10 +2,4 @@
 
 // {{ ansible_managed }}
 
-// Disable composer support from Aegir, it can cause surprises
-// when Aegir is updated and runs verify on all platforms (which then updates
-// all platforms).
-$options['provision_composer_install_platforms'] = FALSE;
-
-// https://www.drupal.org/project/provision/issues/2953349#comment-14038063
-$options['provision_db_cloaking'] = FALSE;
+// We do not use this file anymore.

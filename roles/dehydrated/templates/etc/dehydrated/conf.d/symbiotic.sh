1b0729a84ca5b82a64636fa84f67fcc6bff94097
Mathieu Lu <mathieu@symbiotic.coop>
2026-05-01 14:20:31 -0400

    dehydrated: more fixes for the custom conf file

--- roles/dehydrated/templates/etc/dehydrated/conf.d/symbiotic.sh
+++ roles/dehydrated/templates/etc/dehydrated/conf.d/symbiotic.sh
@@ -1,7 +1,7 @@
 # {{ ansible_managed }}
 
 # Run these hooks after renewals
-HOOK="${BASEDIR}/hooks.sh"
+HOOK="/etc/dehydrated/hooks.sh"
 
 # Skip over errors during certificate orders (ex: if one cert is failing, keep going)
 KEEP_GOING=yes
